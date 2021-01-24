c125bc6b2bee4e21c6b52c4d098ed527f08cd7bc
daikichi86 <maskman-daikigx@i.softbank.jp>
2021-01-25 05:11:24 +0900

    devise実装

---
+++ app/controllers/mains_controller.rb
@@ -0,0 +1,10 @@
+class MainsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ spec/requests/mains_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Mains", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/mains/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/mains/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
