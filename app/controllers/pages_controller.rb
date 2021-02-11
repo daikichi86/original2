a5d72b8b653339bd7fb6533c463866fd9d2193ca
daikichi86 <maskman-daikigx@i.softbank.jp>
2021-02-11 22:46:41 +0900

    フォームオブジェクト

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :main_detail_find, only: [:show, :edit, :update, :destroy]
+  before_action :main_find, only: [:show]
 
   def index
     @pages = Main.includes(:user).order("id DESC")
@@ -11,17 +11,27 @@ class PagesController < ApplicationController
   end
 
   def edit
+    main = Main.find(params[:id])
+    detail = Detail.find(@main.id)
+    @main_form = maindetail.new
+    binding.pry
   end
 
   def update
   end
 
   def destroy
+    if current_user.id == @main.user_id
+      @main.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
 
-    def main_detail_find
+    def main_find
       @main = Main.find(params[:id])
       @detail = Detail.find(@main.id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'mains#index'
   resources :mains, only:[:index, :new, :create]
-  resources :pages, only:[:index, :show, :edit, :destroy]
+  resources :pages, only:[:index, :show, :edit, :update, :destroy]
 end
